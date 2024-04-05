b5ede69c2ad9efb9229c05a1a7ac7c6c0710d2e9
Jefferson Bomfim <jefferson_bomfim@outlook.com>
2024-04-05 12:29:53 -0300

    Update Lay.js
    fixes

--- Lay.js
+++ Lay.js
@@ -2,19 +2,40 @@ class Lay {
     constructor(grupo,chave) {
         this.key = {...chave};
         this._grupo = grupo;
+        this.defmapf = v => v;
     }
-    where(f) { return this.filter(f) }
-    filter(f) { return this._grupo.filter(f) }
-    toList() { return [...this._grupo] }
-    count(f) { return this.length(f) }
+    where(f) {
+        return this.filter(f)
+    }
+    filter(f) {
+        return this._grupo.filter(f)
+    }
+    toList() {
+        return [...this._grupo]
+    }
+    count(f) {
+        return this.length(f)
+    }
     length(f) {
         if(f) return this.filter(f).length;
         else return this._grupo.length;
     }
-    sum(mapF) { return this._grupo.map(mapF??(v=>v)).reduce((a,v) => a+=v) }
-    min(mapF) { return Math.min(...this._grupo.map(mapF??(v=>v))) }
-    max(mapF) { return Math.max(...this._grupo.map(mapF??(v=>v))) }
-    avg(mapF) { return this.sum(mapF??(v=>v))/this.length() }
+    sum(mapF) {
+        mapF = mapF === null ? this.defmapf : mapF;
+        return this._grupo.map(mapF).reduce((a,v) => a+=v);
+    }
+    min(mapF) {
+        mapF = mapF === null ? this.defmapf : mapF;
+        return Math.min(...this._grupo.map(mapF));
+    }
+    max(mapF) {
+        mapF = mapF === null ? this.defmapf : mapF;
+        return Math.max(...this._grupo.map(mapF));
+    }
+    avg(mapF) {
+        mapF = mapF === null ? this.defmapf : mapF;
+        return this.sum(mapF)/this.length();
+    }
     first(f) { 
         if(f) return this.filter(f)[0];
         else return this._grupo[0];
@@ -38,9 +59,7 @@ class Lay {
             const filtered = this.filter(f);
             return filtered.length > 0 ? filtered[filtered.length-1] : null;
         }
-        else {
-            return this._grupo.length > 0 ? this._grupo[this._grupo.length-1] : null;
-        }
+        else return this._grupo.length > 0 ? this._grupo[this._grupo.length-1] : null;
     }
 
     groupBy(groupBy) {
@@ -59,7 +78,7 @@ class Lay {
     static group(listOfObjects, groupBy, selectField) {
         const ungroupedKeys = listOfObjects.map(o => Object.values(groupBy(o)).join(','));
         const keys = ungroupedKeys.filter((f, i) => ungroupedKeys.indexOf(f) === i);
-        selectField ??= (v=>v.key);
+        selectField = selectField === null ? (v=>v.key) : selectField;
         const final = keys.map(key => {
             let grupo = listOfObjects.filter(f => key === Object.values(groupBy(f)).join(','));
             let chave = [grupo[0]].map(groupBy)[0];
